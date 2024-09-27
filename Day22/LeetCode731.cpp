480703f708a1014f31ad7012ebc315adf16ca16e
LordOfBlueFlame <Cepkamil64@gmail.com>
2024-09-27 22:47:03 +0200

    731. My Calendar II

---
+++ Day22/LeetCode731.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <utility>
+
+class MyCalendarTwo {
+private:
+    std::vector<std::pair<int, int>> singleBookings;
+    std::vector<std::pair<int, int>> doubleBookings;
+
+public:
+    MyCalendarTwo() {}
+
+    bool book(int start, int end) {
+        for (const auto& interval : doubleBookings) {
+            if (start < interval.second && end > interval.first) {
+                return false;
+            }
+        }
+
+        for (const auto& interval : singleBookings) {
+            if (start < interval.second && end > interval.first) {
+                doubleBookings.push_back({std::max(start, interval.first), std::min(end, interval.second)});
+            }
+        }
+
+        singleBookings.push_back({start, end});
+
+        std::sort(doubleBookings.begin(), doubleBookings.end());
+
+        return true; 
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
