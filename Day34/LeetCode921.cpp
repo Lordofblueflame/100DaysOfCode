f245515449161ccfcad1cb913d13941e79e91cf3
LordOfBlueFlame <Cepkamil64@gmail.com>
2024-10-09 22:24:05 +0200

    LeetCode: 921. Minimum Add to Make Parentheses Valid

---
+++ Day34/LeetCode921.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class Solution {
+public:
+    int minAddToMakeValid(std::string s) {
+        int open = 0;
+        int close = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                open++;
+            } else { 
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++;
+                }
+            }
+        }
+        
+        return open + close;
+    }
+};
